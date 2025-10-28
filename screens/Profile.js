0b32f2767548ed5f7b89a74d4639fbe2e084ea6a
Yoann9808 <yoann.pouliquen92@gmail.com>
2025-10-28 09:45:53 +0100

    update

--- screens/Profile.js
+++ screens/Profile.js
@@ -7,8 +7,6 @@ import { logout } from "../reducers/users";
 
 const Stack = createNativeStackNavigator();
 
-// hello
-
 export default function Profile({ navigation }) {
   const dispatch = useDispatch();
   const token = useSelector((state) => state.users.value.token);
