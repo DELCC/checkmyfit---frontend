1aa19ce7c7415bb0ef05115fc5306b617c0d88f0
Yoann9808 <yoann.pouliquen92@gmail.com>
2025-10-28 12:52:54 +0100

    login, signup update

--- screens/Signup.js
+++ screens/Signup.js
@@ -17,33 +17,50 @@ export default function Signup({ navigation }) {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
   const dispatch = useDispatch();
 
   const ipAdress = "192.168.100.144:3000";
 
+  const EMAIL_REGEX =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/; // Minimum 1 letter, 1 number and at least 8 characters
+
   const handleSignup = () => {
-    fetch(`http://${ipAdress}/users/signup`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, email, password }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.token) {
-          dispatch(addUserToStore(data.token));
-          setEmail("");
-          setPassword("");
-          setUsername("");
-          navigation.navigate("Home", {
-            isNewUser: true,
-          });
-        } else {
-          console.log("incorrect password or username");
-        }
-      });
+    if (EMAIL_REGEX.test(email) && PASSWORD_REGEX.test(password)) {
+      // passer à la suite
+      fetch(`http://${ipAdress}/users/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.token) {
+            dispatch(addUserToStore(data.token));
+            setEmail("");
+            setPassword("");
+            setUsername("");
+            navigation.navigate("Home", {
+              isNewUser: true,
+            });
+          } else {
+            console.log("no token");
+          }
+        });
+    } else {
+      console.log(`Password or email incorrect format`);
+      setEmailError(true);
+      setPasswordError(true);
+      setEmail("");
+      setPassword("");
+      setUsername("");
+    }
   };
 
   return (
@@ -58,10 +75,11 @@ export default function Signup({ navigation }) {
         <View style={styles.inputGroup}>
           <Text style={styles.inputLabel}>Username</Text>
           <TextInput
-            placeholder="your@email.com"
             style={[styles.input, styles.inputRounded]}
             placeholderTextColor="#9ca3af"
-            keyboardType="email-address"
+            keyboardType="default"
+            autoCapitalize="none"
+            autoCorrect={false}
             onChangeText={(value) => setUsername(value)}
             value={username}
           />
@@ -74,6 +92,8 @@ export default function Signup({ navigation }) {
             style={[styles.input, styles.inputRounded]}
             placeholderTextColor="#9ca3af"
             keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
             onChangeText={(value) => setEmail(value)}
             value={email}
           />
@@ -85,12 +105,23 @@ export default function Signup({ navigation }) {
             placeholder="Enter your password"
             style={[styles.input, styles.inputRounded]}
             placeholderTextColor="#9ca3af"
-            keyboardType="email-address"
+            keyboardType="default"
+            autoCapitalize="none"
+            autoCorrect={false}
             secureTextEntry={true}
             onChangeText={(value) => setPassword(value)}
             value={password}
           />
         </View>
+
+        {emailError && passwordError && (
+          <View style={styles.errorContainer}>
+            <Text style={styles.errorText}>
+              Oups ! Entrez un e-mail valide et un mot de passe d’au moins 8
+              caractères avec une lettre et un chiffre.
+            </Text>
+          </View>
+        )}
 
         <TouchableOpacity
           style={[styles.button, styles.primaryButton]}
@@ -177,5 +208,22 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "#00A6A6",
     textDecorationLine: "underline",
+  },
+  errorContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#FDECEC",
+    borderLeftWidth: 4,
+    borderLeftColor: "#D9534F",
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  errorText: {
+    color: "#A94442",
+    fontSize: 14,
+    flex: 1,
+    fontWeight: "500",
   },
 });
